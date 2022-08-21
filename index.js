1146a957268fe7500b706ba72f9708065d0ce7b0
codingvaleria <codingvaleria@gmail.com>
2022-08-21 21:28:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const item = record.find(findYear)
+    if (item) {
+        return item.year;
+    }
+}
+function findYear(record) {
+
+    if (record.result === "W") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+
+
+
